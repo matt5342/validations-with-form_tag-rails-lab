7b4ae7641304ccf8f9df2b0935d9fc860bdd6abd
Matt <mattfsewell@gmail.com>
2021-02-11 13:52:59 -0500

    commit tree 44febf08b9361b3bf952fb096fbe11bce76c1dcc

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,14 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-
     @post.update(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
 
-    redirect_to post_path(@post)
   end
 
   private
